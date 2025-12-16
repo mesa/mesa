051bedecb51dc8ef79cd13a4c4546d584793615c
Nithurshen <nithurshen@gmail.com>
2025-12-16 09:56:50 +0530

    fix: ruff format

--- mesa/examples/advanced/epstein_civil_violence/model.py
+++ mesa/examples/advanced/epstein_civil_violence/model.py
@@ -1,5 +1,5 @@
 import mesa
-from mesa.discrete_space import OrthogonalMooreGrid, OrthogonalVonNeumannGrid #
+from mesa.discrete_space import OrthogonalMooreGrid, OrthogonalVonNeumannGrid  #
 from mesa.examples.advanced.epstein_civil_violence.agents import (
     Citizen,
     CitizenState,
