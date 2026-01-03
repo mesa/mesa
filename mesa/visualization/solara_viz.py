a0572bca7d7f8fed853eb34b22597633e2d7011a
Aman Bihari <161295600+codebreaker32@users.noreply.github.com>
2026-01-03 22:58:26 +0530

    Refactoring solara_viz

--- mesa/visualization/solara_viz.py
+++ mesa/visualization/solara_viz.py
@@ -276,7 +276,6 @@ def SpaceRendererComponent(
             renderer.draw_propertylayer()
 
         viz_dependencies = [update_counter.value]
-
         # Update the fig every time frame
         if dependencies:
             viz_dependencies.extend(dependencies)
