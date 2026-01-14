c402c98f7f7e638c292d58d4cb6c410e8562f628
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2026-01-14 21:29:37 +0100

    Update __init__.py

--- mesa/experimental/scenarios/__init__.py
+++ mesa/experimental/scenarios/__init__.py
@@ -1,5 +1,5 @@
 """Scenarios module."""
 
-from .scenario import Scenario, scenarios_from_dataframe, scenarios_from_numpy
+from .scenario import Scenario
 
-__all__ = ["Scenario", "scenarios_from_dataframe", "scenarios_from_numpy"]
+__all__ = ["Scenario",]
