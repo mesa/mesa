757871d9d20868e77e8a7b494ed2a6fba8a91343
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2026-01-21 22:06:20 +0100

    Update statistics.py

--- mesa/experimental/statistics.py
+++ mesa/experimental/statistics.py
@@ -364,7 +364,7 @@ class NumpyAgentDataSet[A: Agent](DataSet):
         # Boolean indexing - returns only active rows
         # self._n_slots is the number of slots that have been handed out
         # some might be inactive now, but there can never be more
-        return self._agent_data[self._is_active[:self._n_slots]
+        return self._agent_data[self._is_active[:self._n_slots]]
 
     @property
     def active_agents(self) -> list[A]:
