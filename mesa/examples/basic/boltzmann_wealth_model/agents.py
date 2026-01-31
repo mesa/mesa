b9b7bd12dfee6a4dde28972baeb6b4434bc6dae9
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2026-01-31 18:47:31 +0100

    Update agents.py

--- mesa/examples/basic/boltzmann_wealth_model/agents.py
+++ mesa/examples/basic/boltzmann_wealth_model/agents.py
@@ -10,9 +10,6 @@ class MoneyAgent(CellAgent):
     Attributes:
         wealth (int): The agent's current wealth (starts at 1)
     """
-
-    # wealth = DataField("wealth", "wealth")
-
     def __init__(self, model, cell):
         """Create a new agent.
 
