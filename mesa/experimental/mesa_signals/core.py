a95bd5bdf87081b18a96822f17baebdb7652111c
Aman <anni18938@gmail.com>
2026-02-07 14:55:26 +0000

    Fix docstring

--- mesa/experimental/mesa_signals/core.py
+++ mesa/experimental/mesa_signals/core.py
@@ -5,8 +5,9 @@ functionality:
 
 - BaseObservable: Abstract base class defining the interface for all observables
 - Observable: Main class for creating observable properties that emit change signals
-- computed: Decorator for creating properties that automatically update based on dependencies
+- computed_property: Decorator for creating properties that automatically update based on dependencies
 - HasObservables: Mixin class that enables an object to contain and manage observables
+- emit: Decorator for methods that emit signals
 - All: Helper class for subscribing to all signals from an observable
 - SignalType: Enum defining the types of signals that can be emitted
 
