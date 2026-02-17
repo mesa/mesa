49b117785c53618a27d2c8d7b5e20d4ad85e7dd2
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2026-02-17 19:48:31 +0100

    Apply suggestion from @quaquel

--- mesa/model.py
+++ mesa/model.py
@@ -184,7 +184,7 @@ class Model[A: Agent, S: Scenario](HasObservables):
         """
         if until <= self.time:
             warnings.warn(
-                f"end_time {end_time} is larger than time {self.time}",
+                f"end time {until} is larger than time {self.time}",
                 RuntimeWarning,
                 stacklevel=2,
             )
