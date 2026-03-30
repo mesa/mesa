1f283bc8518bc89fcf4e9fa939d787974658549b
Riya3024 <riyapanditstm@gmail.com>
2026-03-30 20:41:57 +0530

    Fix: remove side effects from ActionSpace module

---
+++ mesa/experimental/action_space/__init__.py
@@ -0,0 +1,5 @@
+from .action import Action
+from .constraint import Constraint
+from .action_space import ActionSpace
+
+__all__ = ["Action", "Constraint", "ActionSpace"]
--- mesa/experimental/action_space/action.py
+++ mesa/experimental/action_space/action.py
@@ -2,6 +2,3 @@ class Action:
     def __init__(self, name, **params):
         self.name = name
         self.params = params
-
-    def __repr__(self):
-        return f"Action({self.name}, {self.params})"
