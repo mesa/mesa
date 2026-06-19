c65969957ed72932c4c19d101ffdf11f709490ce
Jan Kwakkel <j.h.kwakkel@tudelft.nl>
2026-06-19 09:18:15 +0200

    add unit tests

--- mesa/experimental/scenarios/__init__.py
+++ mesa/experimental/scenarios/__init__.py
@@ -1,6 +1,7 @@
 """Scenarios module."""
 
 from .scenario import Scenario
+from .runner import RunConfiguration
 
 __all__ = [
     "Scenario",
--- mesa/experimental/scenarios/runner.py
+++ mesa/experimental/scenarios/runner.py
@@ -1,10 +1,14 @@
 """Classes for running parameter sweeps over scenarios."""
-from typing import Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
 
 import pandas as pd
 
-from mesa import Model
 from mesa.experimental.scenarios import Scenario
+
+if TYPE_CHECKING:
+    from mesa.model import Model
 
 
 class RunConfiguration:
@@ -24,7 +28,7 @@ class RunConfiguration:
 
     """
     def __init__(self, model_class: type[Model], until:float | int, model_args:None| list[Any]=None,
-                 model_kwargs:None|dict[str, Any]=None, outcomes: None|str|list[str]=None):
+                 model_kwargs:None|dict[str, Any]=None, outcomes: None|str|list[str]=None, data_recorder_attr_name="data_recorder"):
         """Initialize a RunConfiguration object.
 
         Args:
@@ -33,9 +37,12 @@ class RunConfiguration:
             model_args: any additional model arguments
             model_kwargs: any additional model keyword arguments
             outcomes: the outcomes to extract. If None, extract all outcomes.
+            data_recorder_attr_name : the name of the data recorder attribute to use on the model
         """
         super().__init__()
 
+        # we need to avoid circular imports
+        from mesa.model import Model  # noqa: PLC0415
         if not (isinstance(model_class, type) and issubclass(model_class, Model)):
             raise TypeError("model_class must be a subclass of Model")
         if not isinstance(until, (int, float)):
@@ -47,6 +54,10 @@ class RunConfiguration:
         self.model_args = [] if model_args is None else model_args
         self.model_kwargs = {} if model_kwargs is None else model_kwargs
         self.until = until
+
+        # fixme:: this code leaves it to the user to set the attribute to which the recorder is assigned
+        #   this is probably a a convention that needs to be pinned down explicitly on the model class
+        self.data_recorder_attr_name = data_recorder_attr_name
 
         if isinstance(outcomes, str):
             outcomes = [outcomes]
@@ -62,12 +73,12 @@ class RunConfiguration:
 
     def extract_output(self, model: Model) -> dict[str, pd.DataFrame]:
         """Extract output from model."""
-        # fixme:: this code assumed that the recorder is assigned to model.data_recorder
-        #   this is probably a a convention that needs to be pinned down explicitly on the model class
+        recorder = getattr(model, self.data_recorder_attr_name)
+
         if self.outcomes is None:
-            return model.data_recorder.get_all_dataframes()
+            return recorder.get_all_dataframes()
         else:
-            return {k:model.data_recorder.get_table_dataframe(k) for k in self.outcomes}
+            return {k:recorder.get_table_dataframe(k) for k in self.outcomes}
 
     def __call__(self, scenario: Scenario) -> dict[str, pd.DataFrame]:
         """Run the scenario and extract output."""
